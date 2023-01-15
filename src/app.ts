43f5f76e06f3c4e83739feb81d957f71f55af3ea
farah2003 <farah.shaqoura@example.com>
2023-01-15 16:41:18 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import { serverError } from './middlewares/error/serverError';
+import express, { Application } from 'express';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import route from './routes';
+import { notFoundError } from './middlewares';
+const app: Application = express();
+
+app.use([
+  compression(),
+  morgan('dev'),
+  cookieParser(),
+  express.urlencoded({ limit: '10mb', extended: true }),
+  express.json({
+    limit: '10mb',
+  }),
+  cors(),
+]);
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1/', route);
+app.use(notFoundError);
+app.use(serverError);
+
+export default app;
