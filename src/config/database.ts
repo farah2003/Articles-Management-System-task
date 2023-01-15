ad419e97d6ceb24e519d0355646e4a47f307838f
farah2003 <farah.shaqoura@example.com>
2023-01-15 17:55:17 +0200

    remove comment

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,15 +5,12 @@ const databaseSchema = joi
   })
   .unknown();
 
-/// why we need unknown because if there all the envoirment variable withour DB_URL allow to be and at give as this error database validate error ValidationError: "GJS_DEBUG_TOPICS" is not allowed
-///the return value here is not turec
 interface DatabaseUrl {
   url: string;
 }
 
 export const database = (): DatabaseUrl => {
   const { error, value } = databaseSchema.validate(process.env);
-  // enven if there is an error there will be a value so that we search about error first and thorw to not continue
 
   if (error) throw new Error(`database validate error ${error}`);
   return {
--- src/database/init.sql
+++ src/database/init.sql
@@ -34,34 +34,34 @@ CREATE TABLE ratings(
   PRIMARY KEY(scoring_by,article_id)
 );
 
-INSERT INTO users (username, email, password) VALUES 
-  ('farah', 'farah@gmail.com', 'fhgfhgfhgjghjhgjj');
+-- INSERT INTO users (username, email, password) VALUES 
+--   ('farah', 'farah@gmail.com', 'fhgfhgfhgjghjhgjj');
 
-INSERT INTO articles(title, contant,  image, email,auther) VALUES 
-  ('article 1', 'This is the first article', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 'farah@gmail.com','farah'),
-    ('article 2', 'This is the first article', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 'farah@gmail.com','farah'),
-      ('article 3', 'This is the first article', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 'farah@gmail.com','farah');
+-- INSERT INTO articles(title, contant,  image, email,auther) VALUES 
+--   ('article 1', 'This is the first article', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 'farah@gmail.com','farah'),
+--     ('article 2', 'This is the first article', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 'farah@gmail.com','farah'),
+--       ('article 3', 'This is the first article', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 'farah@gmail.com','farah');
 
-INSERT INTO comments (contant, created_by,article_id) VALUES
-('This is the first comment', 1,1 ),
-('This is the first comment', 1,1 ),
-('This is the first comment', 1,1 ),
-('This is the first comment', 1,1 ),
-('This is the first comment', 1,1 ),
-('This is the first comment', 1,1 ),
-('This is the first comment', 1,1 ),
-('This is the first comment', 1,1 );
+-- INSERT INTO comments (contant, created_by,article_id) VALUES
+-- ('This is the first comment', 1,1 ),
+-- ('This is the first comment', 1,1 ),
+-- ('This is the first comment', 1,1 ),
+-- ('This is the first comment', 1,1 ),
+-- ('This is the first comment', 1,1 ),
+-- ('This is the first comment', 1,1 ),
+-- ('This is the first comment', 1,1 ),
+-- ('This is the first comment', 1,1 );
 
 
 
-INSERT INTO comments (contant, created_by,article_id) VALUES
-('This is the first comment', 1,2 ),
-('This is the first comment', 1,2 ),
-('This is the first comment', 1,2 );
+-- INSERT INTO comments (contant, created_by,article_id) VALUES
+-- ('This is the first comment', 1,2 ),
+-- ('This is the first comment', 1,2 ),
+-- ('This is the first comment', 1,2 );
 
-INSERT INTO ratings (scoring_by,article_id,rating) VALUES
-( 1,1,3),
-( 1,3,5 );
+-- INSERT INTO ratings (scoring_by,article_id,rating) VALUES
+-- ( 1,1,3),
+-- ( 1,3,5 );
 
 
 COMMIT;
